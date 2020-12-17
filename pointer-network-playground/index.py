13ccd6caa76200cef3651a93dca635468f216b20
kobako <zhuobinggang@gmail.com>
2020-12-17 12:58:37 +0900

    dd

--- pointer-network-playground/index.py
+++ pointer-network-playground/index.py
@@ -1,45 +1,94 @@
 import torch as t
 
-input_range = [0,2020] # min, max
+input_range = [0,2000] # min, max
+num_embeddings = 2020
 # embedding_dim = 9 # Number embedding dimension
 input_size = 9
-hidden_size = 12
+hidden_size = input_size
 ember = None # Torch embedding layer
-lstm = None # Encoder
+encoder = None
+decoder = None
+# 2000以上的数字用于特殊用途
+SOF = 2001
+EOF = 2002
 
 def neural_sort(nums):
   print(sorted(nums))
 
 # input: [5,3,2,6] //number(int) sequence
+# output: emb([0,5,3,2,6])
 def embeddings_from_ints(nums):
   global ember, input_range, input_size
   # Check for ember
   if ember is None:
-    ember = t.nn.Embedding(input_range[1], input_size)
+    ember = t.nn.Embedding(num_embeddings, input_size)
   else:
     pass
   # turn nums to embeddings
   ts = t.LongTensor(nums)
   return ember(ts)
 
-# input: (seq_len, inpt_size)
+# input: (seq_len, input_size)
 def add_fake_batch(embs_of_nums):
   seq_len, inpt_size = embs_of_nums.shape
   return embs_of_nums.reshape(seq_len, 1, inpt_size)
 
-# TODO: Q: Should I output all [h] for attention?
+def inpt_for_lstm(nums):
+  return add_fake_batch(embeddings_from_ints(nums))
+
+
+# output: [[input_size]]
+def SOF_embedded():
+  global ember
+  if ember is None:
+    print('You should prepare an ember before using this function')
+  return ember(t.LongTensor([SOF]))
+
+def EOF_embedded():
+  global ember
+  if ember is None:
+    print('You should prepare an ember before using this function')
+  return ember(t.LongTensor([EOF]))
+
+
+# output: (out, (hn, cn))
 def encode(inpt):
-  if lstm is None:
-    lstm = t.nn.LSTM(input_size, hidden_size)
+  global encoder
+  # create encoder if not ready
+  if encoder is None:
+    encoder = t.nn.LSTM(input_size, hidden_size)
   else:
     pass
-  # Make h0,c0, and feed in LSTM
-  h0 = t.randn(1,1, hidden_size)
+  # EOF as h0
+  h0 = add_fake_batch(EOF_embedded())
   c0 = t.randn(1,1, hidden_size)
-  return lstm(inpt, (h0, c0))
+  return encoder(inpt, (h0, c0))
+
+
+def first_step_decode(hn):
+  h0 = hn
+  c0 = t.randn(1,1, hidden_size)
+  return step_decode(add_fake_batch(SOF_embedded()), h0, c0)
+
+def after_first_step(hn):
+  _,(h1,_) = first_step_decode(hn)
+  
+
+def step_decode(inpt, h0, c0):
+  global decoder 
+  if decoder is None:
+    decoder = t.nn.LSTM(input_size, hidden_size)
+  else:
+    pass
+  return decoder(inpt, (h0, c0))
+
+
+# TODO: 
+def index_from_hidden_state_by_argmax(hidden_state, inpt_embs, SOF_emb):
+  pass
   
 def decode():
-  pass 
+  pass
 
 
 # inpt: (batch, input_size)
@@ -47,23 +96,17 @@ def wrap_fake_seq(inpt):
   batch, input_size = inpt.shape
   return inpt.reshape(1, batch, input_size)
 
-def test_lstm_every_step():
-  inpt = add_fake_batch(embeddings_from_ints([5, 3, 2]))
-  lstm = t.nn.LSTM(input_size, hidden_size)
-  h0 = t.randn(1,1, hidden_size)
-  c0 = t.randn(1,1, hidden_size)
-  out,(hn, cn) = lstm(inpt, (h0, c0))
-  # By step
-  inpt0 = wrap_fake_seq(inpt[0])
-  inpt1 = wrap_fake_seq(inpt[1])
-  inpt2 = wrap_fake_seq(inpt[2])
-  out1, (h1, c1) = lstm(inpt0, (h0, c0))
-  out2, (h2, c2) = lstm(inpt1, (h1, c1))
-  out3, (h3, c3) = lstm(inpt2, (h2, c2))
-  print('Please check that they are the same')
-  print(out3)
-  print(out[2])
-  print(h3)
-  
+def test():
+  inpt = inpt_for_lstm([5,3,2])
+  _, (hn, _) = encode(inpt)
+  _, (dh1, _) = first_step_decode(hn)
+  layer_dh_to_query = t.nn.Linear(hidden_size, input_size)
+  query = layer_dh_to_query(dh1)
+  # concat input with EOF
+  eof = add_fake_batch(EOF_embedded())
+  inpts_for_attend = t.cat((eof, inpt))
+  # all inputs dot query then softmax
+  seq_vs_similarity = t.mm(inpts_for_attend.view(-1, input_size), query.view(input_size, 1))
+
 
 
