5a7c8bf9d11151b0f4c10b5ad7fc18b36c97455e
kobako <zhuobinggang@gmail.com>
2021-03-12 12:46:23 +0900

    增加TF实验设置

--- sector/exp4.py
+++ sector/exp4.py
@@ -1,4 +1,82 @@
 from csg_exp3 import *
 
-def fuck():
-  pass
+class BERT_LONG_TF(BERT_LONG_DEPEND):
+  def init_hook(self):
+    self.self_att_layer = nn.TransformerEncoderLayer(d_model=self.bert_size, nhead=1, dim_feedforward=int(self.bert_size * 1.5), dropout=0.1)
+    self.classifier = nn.Sequential( # (1, 768) => (1, 2)
+      nn.Linear(self.bert_size, 2),
+    )
+    self.CEL = nn.CrossEntropyLoss(t.FloatTensor([1, 3]))
+    # self.CEL = nn.CrossEntropyLoss(t.FloatTensor([1, 4])) # LSTM比较难训练，试着
+
+  def get_should_update(self):
+    return chain(self.bert.parameters(), self.classifier.parameters(), self.self_att_layer.parameters())
+
+  # ss: (sentence_size, 768)
+  # return: (sentence_size, 768)
+  def integrate_sentences_info(self, ss):
+    seq_len, feature = ss.shape
+    ss = ss.view(seq_len, 1, feature) # (sentence_size, 1, 768)
+    ss = self.self_att_layer(ss) # (sentence_size, 1, 768)
+    return ss.view(seq_len, feature)
+
+  # inpts: token_ids, attend_marks
+  # token_ids: (sentence_size, max_id_len)
+  # labels: (sentence_size), zero/one
+  def train(self, inpts, labels):
+    token_ids, attend_marks = inpts # token_ids = attend_marks: (sentence_size, max_id_len)
+    # labels = self.preprocess_labels(labels)
+    label, pos = labels # LongTensor([label/pos])
+    pos = pos.item()
+    if GPU_OK:
+      token_ids = token_ids.cuda()
+      attend_marks = attend_marks.cuda()
+      label = label.cuda()
+    embs = self.get_batch_cls_emb(token_ids, attend_marks) # (sentence_size, 768)
+    embs = self.processed_embs(embs) # (sentence_size, 768)
+
+    o = self.integrate_sentences_info(embs) # (sentence_size, 768)
+    o = o[pos] # (768)
+    o = o.view(1, self.bert_size) # (1, 768)
+    o = self.classifier(o) # (1, 2)
+    loss = self.CEL(o, label)
+
+    self.zero_grad()
+    loss.backward()
+    self.optim.step()
+    self.print_train_info(o, label, loss.detach().item())
+
+    return loss.detach().item()
+
+  @t.no_grad()
+  def dry_run(self, inpts, labels=None):
+    token_ids, attend_marks = inpts # token_ids = attend_marks: (sentence_size, max_id_len)
+    # labels = self.preprocess_labels(labels)
+    label, pos = labels # LongTensor([label/pos])
+    pos = pos.item()
+    if GPU_OK:
+      token_ids = token_ids.cuda()
+      attend_marks = attend_marks.cuda()
+      label = label.cuda()
+    embs = self.get_batch_cls_emb(token_ids, attend_marks) # (sentence_size, 768)
+    embs = self.processed_embs(embs) # (sentence_size, 768)
+
+    o = self.integrate_sentences_info(embs) # (sentence_size, 768)
+    o = o[pos] # (768)
+    o = o.view(1, self.bert_size) # (1, 768)
+    o = self.classifier(o) # (1, 2)
+
+    self.print_train_info(o, label, -1)
+    return o.view(-1).argmax().item()
+
+# =============== 
+
+def get_datas_long_depend_baseline(length = 4):
+  G['ld'] = ld = Loader_Long_Depend(Train_DS_Long_Depend(ss_len = length))
+  G['testld'] = testld = Loader_Long_Depend(Test_DS_Long_Depend(ss_len = length))
+  G['devld'] = devld = Loader_Long_Depend(Dev_DS_Long_Depend(ss_len = length))
+  m = G['m'] = BERT_LONG_TF()
+  losses = runner.train_simple(m, ld, 1) # only one epoch for order matter model
+  G['testdic'] = runner.get_test_result_long(m, testld)
+  G['devdic'] = runner.get_test_result_long(m, devld)
+  return losses
