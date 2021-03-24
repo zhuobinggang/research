cdf420be95933f9428e8c9defa27f7a3130fdfc7
zhuobinggang <zhuobinggang@gmail.com>
2021-03-24 14:12:05 +0900

    wiki fucker

--- sector/word2vec_fucker.py
+++ sector/word2vec_fucker.py
@@ -3,8 +3,8 @@ import unidic_lite
 import MeCab
 
 tagger = MeCab.Tagger(unidic_lite.DICDIR)
-MODEL_FILE = '/home/taku/projects/jawiki_20180420_300d.pkl'
-# MODEL_FILE = '/usr01/ZhuoBinggang/jawiki_20180420_300d.pkl'
+# MODEL_FILE = '/home/taku/projects/jawiki_20180420_300d.pkl'
+MODEL_FILE = '/usr01/ZhuoBinggang/jawiki_20180420_300d.pkl'
 wiki2vec = Wikipedia2Vec.load(MODEL_FILE)
 
 def sentence_to_words(s, max_len = 512):
