9d924391be8667618d6966a7bf30061d12b21b8f
kobako <zhuobinggang@gmail.com>
2021-04-07 16:14:06 +0900

    dd

--- sector/order.py
+++ sector/order.py
@@ -213,16 +213,17 @@ def run_save_dryrun():
       'ordering_result': U.cal_prec_rec_f1_v2(m.dry_run_output, m.dry_run_labels)
     }, 'dd')
 
+
 def run():
   for i in range(20): 
     init_G_Symmetry(2, sgd = True, batch = 2)
     G['m'] = m = Sector_SEP_Order_CLS(rate=0) # 2 vs 2, ordering
-    get_datas(i + 40, 2, f'2vs2, plus ordering')
+    get_datas(i + 40, 1, f'2vs2, plus ordering')
     init_G_Symmetry(1, sgd = True, batch = 4)
     G['m'] = m = Double_Sentence_CLS(rate=0) # 1 vs 1
-    get_datas(i, 2, f'1vs1')
+    get_datas(i, 1, f'1vs1')
     init_G_Symmetry(2, sgd = True, batch = 2)
     G['m'] = m = Double_Sentence_CLS(rate=0) # 2 vs 2
-    get_datas(i + 20, 2, f'2vs2')
+    get_datas(i + 20, 1, f'2vs2')
 
     
