f1986d20c540ba032d8424adf71022a5e8d3f8e0
taku <zhuobinggang@gmail.com>
2021-08-11 14:57:11 +0900

    dd

--- sector/sec_bootstrap_save.py
+++ sector/sec_bootstrap_save.py
@@ -11,35 +11,35 @@ g_save = {
         'outputs_FL':[] 
 }
 
-def cal_results(start_index = 0):
+def cal_results(start_index = 0, cnt = 10):
     global g_save
     # stand
-    for i in range(5):
+    for i in range(cnt):
         idx = start_index + i
         m = t.load(f'save/stand_{idx}.tch')
         outputs, targets = get_test_result(m, tld)
         g_save['targets'] = targets
         g_save['outputs_stand'].append(outputs)
     # my
-    for i in range(5):
+    for i in range(cnt):
         idx = start_index + i
         m = t.load(f'save/my_{idx}.tch')
         outputs, targets = get_test_result(m, tld)
         g_save['targets'] = targets
         g_save['outputs_my'].append(outputs)
     # fl
-    for i in range(5):
+    for i in range(cnt):
         idx = start_index + i
         m = t.load(f'save/fl_{idx}.tch')
         outputs, targets = get_test_result(m, tld)
         g_save['targets'] = targets
         g_save['outputs_FL'].append(outputs)
     # save
-    save_g(start_index)
+    save_g(start_index, cnt)
 
-def save_g(start_index):
+def save_g(start_index, cnt):
     global g_save
-    f = open(f'bootstrap_results_start_from_{start_index}.txt', 'w')
+    f = open(f'bootstrap_results_start_from_{start_index}_cnt_{cnt}.txt', 'w')
     f.write(str(g_save))
 
 
