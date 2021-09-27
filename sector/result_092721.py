2a568dd70f8b8f106cc52aefd010de6df9e8e822
taku <zhuobinggang@gmail.com>
2021-09-27 16:51:38 +0900

    增加实验设置

--- sector/result_092721.py
+++ sector/result_092721.py
@@ -1,10 +1,28 @@
 import numpy as np
 
-from mess import *
+from mess_pc import *
+from mess_pt import *
 
 def get_mat(results, key = 'f1'):
+    # print(key)
     mat = []
     for item in results:
         mat.append((item[key], item['test_result'][key]))
     mat = np.array(mat)
     return mat
+
+def split_from_res(res, num=13, step=20):
+    idx = 0
+    results_dev = []
+    results_test = []
+    for i in range(num):
+        idx += i * step
+        results_dev.append(get_mat(res[idx:idx+20]).mean(0)[0])
+        results_test.append(get_mat(res[idx:idx+20]).mean(0)[1])
+    return results_dev, results_test
+
+def get_heatmap():
+    xs = ['r00+fl10', 'r00+fl20', 'r00+fl05', 'r01+fl10','r01+fl20','r01+fl05','r02+fl10','r02+fl20', 'r02+fl05', 'stand', 'fl10', 'fl20', 'fl05']
+    ys_dev_pc, ys_test_pc = split_from_res(res_pc)
+    ys_dev_pt, ys_test_pt = split_from_res(res_pt)
+    return xs, [ys_dev_pc, ys_test_pc, ys_dev_pt, ys_test_pt]
--- sector/result_ploter.py
+++ sector/result_ploter.py
@@ -1,6 +1,7 @@
 # import result_grid2 as R2
 # import result_090521 as R1
-import result_091321 as R3
+# import result_091321 as R3
+import result_092721 as R4
 from plot import *
 import seaborn as sns # for heatmap
 
@@ -11,13 +12,24 @@ def draw():
     draw_line_chart(AUXILIARY_LOSS_XS, [y1, y2], ['2nd', '1st'])
 
 def output_heatmap(mat, xs, ys, path = 'dd.png'):
-  plt.clf()
-  sns.heatmap(mat, xticklabels=xs, yticklabels=ys, cmap='Blues', annot=True, fmt='.4g')
-  plt.savefig(path)
+    plt.clf()
+    sns.heatmap(mat, xticklabels=xs, yticklabels=ys, cmap='Blues', annot=True, fmt='.4g')
+    plt.savefig(path)
+
+def output_bar_chart(xs, yss, path = 'dd.png'):
+    plt.clf()
+    # plt.bar(xs, ys, color ='maroon', width = 0.4)
+    for ys in yss:
+        plt.plot(xs, ys)
+    plt.savefig(path)
 
 # 0913
 def draw_heatmap():
     mat, ys, xs = R3.get_mat()
     output_heatmap(mat, xs, ys)
 
+# 0927
+def draw_heatmap():
+    xs, yss = R4.get_heatmap()
+    output_bar_chart(xs, yss)
 
