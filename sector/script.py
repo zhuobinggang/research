a47e3b813d7aaac9d92482d46235584d08454b9d
taku <zhuobinggang@gmail.com>
2021-10-05 16:53:03 +0900

    增加panther上的实验设置

---
+++ sector/script.py
@@ -0,0 +1,7 @@
+from sec_bootstrap_save import *
+from sec_paragraph import *
+
+def run():
+    cal_results()
+    grid_search_plus()
+
--- sector/sec_bootstrap_save.py
+++ sector/sec_bootstrap_save.py
@@ -2,63 +2,57 @@ from sec_paragraph import *
 import random
 
 init_G_Symmetry_Mainichi(half=2, batch=1, mini=False)
-# tld = G['testld']
-tld = G['devld']
+tld = G['testld']
+# tld = G['devld']
 
 g_save = {
         'tld': tld, 
         'targets': None, 
-        'outputs_stand': [], 
-        'outputs_my': [], 
-        'outputs_FL':[],
-        'outputs_myr0': []
+        'outputs_aux_fl': [], 
+        'outputs_aux': [], 
+        'outputs_fl':[],
+        'outputs_stand': []
 }
 
 def cal_results(start_index = 0, cnt = 10):
     global g_save
-    # stand
+    # aux + fl
+    print('aux + fl')
     for i in range(cnt):
         idx = start_index + i
-        m = t.load(f'save/stand_{idx}.tch')
+        m = t.load(f'save/r01_fl20_{i + start_index}.tch')
+        outputs, targets = get_test_result(m, tld)
+        g_save['targets'] = targets
+        g_save['outputs_aux_fl'].append(outputs)
+    # stand
+    print('stand')
+    for i in range(cnt):
+        idx = start_index + i
+        m = t.load(f'save/stand_{i + start_index}.tch')
         outputs, targets = get_test_result(m, tld)
         g_save['targets'] = targets
         g_save['outputs_stand'].append(outputs)
-    # my
-    for i in range(cnt):
-        idx = start_index + i
-        m = t.load(f'save/my_{idx}.tch')
-        outputs, targets = get_test_result(m, tld)
-        g_save['targets'] = targets
-        g_save['outputs_my'].append(outputs)
-    # my r00
-    for i in range(cnt):
-        idx = start_index + i
-        m = t.load(f'save/my_r00_{idx}.tch')
-        outputs, targets = get_test_result(m, tld)
-        g_save['targets'] = targets
-        g_save['outputs_myr0'].append(outputs)
     # fl
+    print('fl')
     for i in range(cnt):
         idx = start_index + i
-        m = t.load(f'save/fl_{idx}.tch')
+        m = t.load(f'save/fl20_{i + start_index}.tch')
         outputs, targets = get_test_result(m, tld)
         g_save['targets'] = targets
-        g_save['outputs_FL'].append(outputs)
+        g_save['outputs_fl'].append(outputs)
+    # aux
+    print('aux')
+    for i in range(cnt):
+        idx = start_index + i
+        m = t.load(f'save/r01_{i + start_index}.tch')
+        outputs, targets = get_test_result(m, tld)
+        g_save['targets'] = targets
+        g_save['outputs_aux'].append(outputs)
     # save
     save_g(start_index, cnt)
 
 def save_g(start_index, cnt):
     global g_save
-    f = open(f'g_save.2v2.txt', 'w')
+    f = open(f'boot_results_by_model.2v2.txt', 'w')
     f.write(str(g_save))
 
-
-def cal_results_new(start_index = 0, cnt = 10):
-    global g_save
-    # stand
-    for i in range(cnt):
-        idx = start_index + i
-        m = t.load(f'save/my_r00_{idx}.tch')
-        outputs, targets = get_test_result(m, tld)
-        g_save['targets'] = targets
-        g_save['outputs'].append(outputs)
