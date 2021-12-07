879b69b1baacd8394f900b83d39d10ce267bec36
taku <zhuobinggang@gmail.com>
2021-12-07 15:57:11 +0900

    实验

--- sector/exp.py
+++ sector/exp.py
@@ -15,12 +15,12 @@ def ld_without_opening(ld):
 
 
 # 让机器来跑特定，取40个平均值
-def get_average(mld):
+def get_average(mld, length = 40):
     res_fl_aux = []
     res_fl = []
     res_bce_aux = []
     res_bce = []
-    for i in range(40):
+    for i in range(length):
         res_fl_aux.append(get_test_result_dic(t.load(f'save/r01_fl50_{i}.tch'), mld))
         res_fl.append(get_test_result_dic(t.load(f'save/fl20_{i}.tch'), mld))
         res_bce.append(get_test_result_dic(t.load(f'save/stand_{i}.tch'), mld))
@@ -28,4 +28,24 @@ def get_average(mld):
     return res_fl_aux, res_fl, res_bce_aux, res_bce
 
 
-# 准备另外100个case的数据集
+def get_n_average(test = False):
+    mld = run()
+    if test:
+        mlds = [mld[:2]]
+        length = 1
+    else:
+        mlds = [mld[:100], mld[100:200], mld[200:300], mld[300:400], mld[400:500]]
+        length = 40
+    res_fl_auxs = []
+    res_fls = []
+    res_bce_auxs = []
+    res_bces = []
+    for mld in mlds:
+        res_fl_aux, res_fl, res_bce_aux, res_bce = get_average(mld, length)
+        res_fl_auxs.append(res_fl_aux)
+        res_fls.append(res_fl)
+        res_bce_auxs.append(res_bce_aux)
+        res_bces.append(res_bce)
+    return res_fl_auxs, res_fls, res_bce_auxs, res_bces
+
+
