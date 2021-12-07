9bd05e0d3e6600fe279f86a8b8ab9c6e473b01cc
taku <zhuobinggang@gmail.com>
2021-12-07 13:33:59 +0900

    add exp

--- sector/exp.py
+++ sector/exp.py
@@ -1,5 +1,6 @@
 # 为了“考察”部分做实验
 import mainichi_paragraph as custom_data
+from manual_exp.mld import mld
 
 def run():
     manual_exp_ld = custom_data.load_customized_loader(file_name='manual_exp', half=2, batch=1, mini=False, shuffle = True)
@@ -11,4 +12,10 @@ def ld_without_opening(ld):
     return ld
 
 
-# 让机器来跑特定
+# 让机器来跑特定，取40个平均值
+results = []
+for i in range(40):
+    results.append(get_test_result_dic(t.load(f'save/r01_fl50_{i}.tch'), mld))
+return results
+
+
