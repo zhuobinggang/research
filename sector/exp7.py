769d96738af2bbaca92a390408751a07a53f6dcd
taku <zhuobinggang@gmail.com>
2022-01-21 15:06:35 +0900

    准备实验

--- sector/exp7.py
+++ sector/exp7.py
@@ -1,5 +1,11 @@
 # 考察：为了证明举例没有意义，对于一个case，验证同一个手法的不同模型得出的结果各异。
 
 from manual_exp.mld import mld
+from sec_paragraph import dry_run_output_posibility
+import torch as t
+
+def beuty_output(m, idx):
+    out, tar = dry_run_output_posibility(m, mld[idx])
+    return round(out.item(), 4), round(tar.item(), 4)
 
 
