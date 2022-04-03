9cd135300a95a0ec936af0841320c9c871f4e0e5
taku <zhuobinggang@gmail.com>
2022-04-03 16:38:47 +0900

    跑实验

--- sector/exp7.py
+++ sector/exp7.py
@@ -6,18 +6,28 @@ from sec_paragraph import dry_run_output_posibility
 from mainichi_paragraph import load_customized_loader
 import torch as t
 import numpy as np
+import requests
 
 # RANGE = (0, 5)
-RANGE = (5, 10)
+LENGTH = 10
+RANGE = (0, LENGTH)
 aux_fl_paths = [f'save/r01_fl50_{i}.tch' for i in range(*RANGE)]
 fl_paths = [f'save/fl20_{i}_e3.tch' for i in range(*RANGE)]
 aux_paths = [f'save/r02_{i}.tch' for i in range(*RANGE)]
 stand_paths = [f'save/stand_{i}.tch' for i in range(*RANGE)]
-LOADER_PATH = 'manual_exp' 
+
+def request_my_logger(dic, desc = 'No describe'):
+  try:
+    url = "https://hookb.in/jeP3mJJdQzF9dlMMmJ7B"
+    dic['desc'] = desc
+    requests.post(url, json=dic)
+  except:
+    print('Something went wrong in request_my_logger()')
 
 
-def load_mld():
-    return load_customized_loader(file_name = LOADER_PATH, half = 2, batch = 1, shuffle = False, mini = False)
+
+def load_mld(path = 'manual_exp'):
+    return load_customized_loader(file_name = path, half = 2, batch = 1, shuffle = False, mini = False)
 
 def beuty_output(m, idx, ld):
     out, tar = dry_run_output_posibility(m, ld[idx])
@@ -98,17 +108,11 @@ def run():
     return [r0, r1, r2, r3], dd
 
 
-# NOTE： 在EXP7上增加考察，计算使用aux之后胜出&败北的例子百分比
-def run_aux_win_and_loss():
-    ld = load_mld()
-    org_idxs_1 = get_all_target_one_idxs(ld)
-    org_idxs_0 = get_all_target_zero_idxs(ld)
-    dd1 = method4_model5_res(org_idxs_1, ld) # (4, len(org_idxs_1), n)
+def get_dd0_dd1(org_idxs_0, org_idxs_1, ld):
     dd0 = method4_model5_res(org_idxs_0, ld) # (4, len(org_idxs_0), n)
-    r1 = best_focus(dd1, -1, ld, org_idxs_1)
-    r0 = best_focus(dd0, -1, ld, org_idxs_0)
-    # THEN: filter_aux_loss_win(r1)
-    return None
+    dd1 = method4_model5_res(org_idxs_1, ld) # (4, len(org_idxs_1), n)
+    return dd0, dd1
+
 
 
 def filter(r, idx):
@@ -192,3 +196,25 @@ def filter_fl_only_loss_0(r):
     return [(global_idxs, case, row) for global_idxs, case, row in r 
         # if row[2] < 0.5 and row[1] > 0.5 and row[3] > 0.5]
         if row[2] > 0.5 and row[1] < 0.5 and row[3] < 0.5]
+
+# NOTE： 在EXP7上增加考察，计算使用aux之后胜出&败北的例子百分比
+def run_aux_win_and_loss():
+    ld = load_mld('test0')
+    org_idxs_1 = get_all_target_one_idxs(ld)
+    org_idxs_0 = get_all_target_zero_idxs(ld)
+    dd1 = method4_model5_res(org_idxs_1, ld) # (4, len(org_idxs_1), n)
+    dd0 = method4_model5_res(org_idxs_0, ld) # (4, len(org_idxs_0), n)
+    r1 = best_focus(dd1, -1, ld, org_idxs_1)
+    r0 = best_focus(dd0, -1, ld, org_idxs_0)
+    dic = {}
+    dic['auxwin1'] = len(filter_aux_only_win_1(r1))
+    dic['auxwin0'] = len(filter_aux_only_win_0(r0))
+    dic['flwin1'] = len(filter_fl_only_win_1(r1))
+    dic['flwin0'] = len(filter_fl_only_win_0(r0))
+    dic['auxlose1'] = len(filter_aux_only_loss_1(r1))
+    dic['auxlose0'] = len(filter_aux_only_loss_0(r0))
+    dic['fllose1'] = len(filter_fl_only_loss_1(r1))
+    dic['fllose0'] = len(filter_fl_only_loss_0(r0))
+    request_my_logger(dic, desc = f"{len(org_idxs_1)}, {len(org_idxs_0)}, 用计算器看看是否跟之前的百分比相近")
+    return r1, r0
+
