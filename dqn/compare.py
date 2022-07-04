ae7263e04ff37024c853f07ad2c703a040d6be64
taku <zhuobinggang@gmail.com>
2022-07-04 18:50:09 +0900

    BUG

--- dqn/compare.py
+++ dqn/compare.py
@@ -2,6 +2,7 @@ from bert_mlp import BERT_MLP, train as train_mlp
 from bert_lstm import BERT_LSTM, train as train_lstm
 from bert_crf import BERT_LSTM_CRF, BERT_MLP_CRF, get_ds, cal_prec_rec_f1_v2, train as train_crf
 from datasets import load_metric
+import torch as t
 # metric = load_metric('seqeval')
 # metric.compute(predictions = y_pred, references = y_true)
 
