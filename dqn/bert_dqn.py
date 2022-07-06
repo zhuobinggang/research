a35adc6e88b7403444516fab7b67e98c0a3a33ee
taku <zhuobinggang@gmail.com>
2022-07-06 13:05:48 +0900

    写了大概的逻辑骨架

--- dqn/bert_dqn.py
+++ dqn/bert_dqn.py
@@ -1,42 +1,59 @@
+import torch as t
+from datasets import load_dataset
+import numpy as np
+nn = t.nn
+F = t.nn.functional
+from transformers import BertTokenizer, BertModel, BertTokenizerFast
+import datetime
+import random
+
+class BERT_LSTM(nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.bert = BertModel.from_pretrained('bert-base-uncased')
+    self.bert.train()
+    self.toker = BertTokenizer.from_pretrained('bert-base-uncased')
+    self.q_net = nn.Sequential(
+            nn.Linear(768, 1024),
+            nn.Tanh(),
+            nn.Linear(1024, 512),
+            nn.Tanh(),
+            nn.Linear(512, 9),
+    )
+    self.GAMMA = 0.99
+    self.cuda()
+
+  def forward(self, obs): # return: (9)
+      # TODO:
+      pass
+
+  def dry_run(self, ids, headword_indexs):
+    out_bert = self.bert(ids.cuda()).last_hidden_state[:, headword_indexs, :] # (1, n, 768)
+    out_lstm, _ = self.lstm(out_bert) # (1, n, 256 * 2)
+    out_mlp = self.mlp(out_lstm) # (1, n, 9)
+    ys = F.softmax(out_mlp, dim = 2) # (1, n, 9)
+    ys = ys.argmax(2).squeeze(0).tolist()
+    return ys
+
+# token_embs: (1, n, 768)
+# labels: (n)
+def logic(m, token_embs, labels, epsilon = 0.2):
+    seq_lenth = token_embs.shape[1]
+    for i in range(seq_lenth)
+        done = (i == seq_lenth - 1)
+        obs = token_embs[i]
+        action = np.random.randint(0, 9) if random.random() <= epsilon else m(obs).argmax().item()
+        q_pred = m(obs)[action]
+        reward = 1 if labels[i] == action else -1
+        if done:
+            with t.no_grad():
+                q_true = m.GAMMA * m(token_embs[i + 1]).max() + reward
+        else: 
+                q_true = reward
+        loss = nn.functional.smooth_l1_loss(q_pred, q_true)
+        # TODO: step back
 
 
-def run():
-  for row_idx, row in enumerate(np.random.permutation(ds_train)):
-      if row_idx % 1000 == 0:
-          # print(f'finished: {row_idx}/{len(ds_train)}')
-          pass
-      tokens_org = row['tokens']
-      # DESC: 每个token可能会被分解成多个subword，所以用headword_indexs来获取开头的subword对应的embedding
-      tokens, ids, headword_indexs = subword_tokenize(tokens_org, m.toker)
-      if tokens is None:
-          print('跳过训练')
-      else:
-          out_bert = bert(ids.cuda()).last_hidden_state[:, headword_indexs, :] # (1, n, 768)
-          
-          out_lstm, _ = m.lstm(out_bert) # (1, n, 256 * 2)
-          out_mlp = m.mlp(out_lstm) # (1, n, 9)
-          ys = F.softmax(out_mlp, dim = 2) # (1, n, 9)
-          # cal loss
-          labels = t.LongTensor(row['ner_tags']) # Long: (n)
-          loss = CEL(ys.squeeze(0), labels.cuda())
-          # backward
-          m.zero_grad()
-          loss.backward()
-          opter.step()
-
-
-def logic():
-    for i in range(len(token_embs))
-        done = (i == len(token_embs) - 1)
-        now_state = token_embs[i]
-        if should_explore:
-            action = sample()
-        else:
-            action = policy(now_state)
-        reward = get_reward(action, i)
-        q_pred = Q(now_state, action)
-        q_true = Q_max(token_embs[i + 1]) + reward
-        loss = BCE(q_pred, q_true)
 
 
 
