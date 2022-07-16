86417d21195ba949143b50b984a493281ab721b4
taku <zhuobinggang@gmail.com>
2022-07-16 14:08:25 +0900

    实验错误，再跑一遍

--- sector/exp_novel.py
+++ sector/exp_novel.py
@@ -44,14 +44,15 @@ dic = {
   'E3FL50': [],
 }
 
-def save_dic():
-    f = open('exp_novel.txt', 'w')
+def save_dic(name = 'exp_novel.txt'):
+    f = open(name, 'w')
     f.write(str(dic))
     f.close()
 
-def run_explore1():
+def run_explore_AUX():
+    PATH = 'run_explore_AUX.txt'
     ld_train = read_ld_train()
-    ld_test = read_ld_test()
+    ld_test = read_ld_dev() # grid search
     # 25 * 3 * 3 * 4 = 900 (mins) = 15 (hours)
     for _ in range(3):
         m = Sector_2022()
@@ -61,7 +62,7 @@ def run_explore1():
         dic['E2FL0AUX00'].append(test_chain(m, ld_test))
         train(m, ld_train, fl_rate = 0, aux_rate = 0)
         dic['E3FL0AUX00'].append(test_chain(m, ld_test))
-        save_dic()
+        save_dic(PATH)
     for _ in range(3):
         m = Sector_2022()
         train(m, ld_train, fl_rate = 0, aux_rate = 0.1)
@@ -70,7 +71,7 @@ def run_explore1():
         dic['E2FL0AUX01'].append(test_chain(m, ld_test))
         train(m, ld_train, fl_rate = 0, aux_rate = 0.1)
         dic['E3FL0AUX01'].append(test_chain(m, ld_test))
-        save_dic()
+        save_dic(PATH)
     for _ in range(3):
         m = Sector_2022()
         train(m, ld_train, fl_rate = 0, aux_rate = 0.2)
@@ -79,7 +80,7 @@ def run_explore1():
         dic['E2FL0AUX02'].append(test_chain(m, ld_test))
         train(m, ld_train, fl_rate = 0, aux_rate = 0.2)
         dic['E3FL0AUX02'].append(test_chain(m, ld_test))
-        save_dic()
+        save_dic(PATH)
     for _ in range(3):
         m = Sector_2022()
         train(m, ld_train, fl_rate = 0, aux_rate = 0.3)
@@ -88,12 +89,12 @@ def run_explore1():
         dic['E2FL0AUX03'].append(test_chain(m, ld_test))
         train(m, ld_train, fl_rate = 0, aux_rate = 0.3)
         dic['E3FL0AUX03'].append(test_chain(m, ld_test))
-        save_dic()
+        save_dic(PATH)
 
 
 def run_explore2():
     ld_train = read_ld_train()
-    ld_test = read_ld_test()
+    ld_test = read_ld_dev() # grid search
     # 25 * 3 * 3 * 4 = 900 (mins) = 15 (hours)
     for _ in range(3):
         m = Sector_2022()
@@ -133,9 +134,10 @@ def run_explore2():
         save_dic()
 
 
-def run_explore3():
+def run_explore_FL():
+    PATH = 'run_explore_FL.txt'
     ld_train = read_ld_train()
-    ld_test = read_ld_test()
+    ld_test = read_ld_dev()
     # 25 * 3 * 3 * 5 = 1125 (mins) = 18.75 (hours)
     for _ in range(3):
         m = Sector_2022()
@@ -145,7 +147,7 @@ def run_explore3():
         dic['E2FL00'].append(test_chain(m, ld_test))
         train_baseline(m, ld_train, epoch = 1, batch = 16, fl_rate = 0)
         dic['E3FL00'].append(test_chain(m, ld_test))
-        save_dic()
+        save_dic(PATH)
     for _ in range(3):
         m = Sector_2022()
         train_baseline(m, ld_train, epoch = 1, batch = 16, fl_rate = 0.5)
@@ -154,7 +156,7 @@ def run_explore3():
         dic['E2FL05'].append(test_chain(m, ld_test))
         train_baseline(m, ld_train, epoch = 1, batch = 16, fl_rate = 0.5)
         dic['E3FL05'].append(test_chain(m, ld_test))
-        save_dic()
+        save_dic(PATH)
     for _ in range(3):
         m = Sector_2022()
         train_baseline(m, ld_train, epoch = 1, batch = 16, fl_rate = 1.0)
@@ -163,7 +165,7 @@ def run_explore3():
         dic['E2FL10'].append(test_chain(m, ld_test))
         train_baseline(m, ld_train, epoch = 1, batch = 16, fl_rate = 1.0)
         dic['E3FL10'].append(test_chain(m, ld_test))
-        save_dic()
+        save_dic(PATH)
     for _ in range(3):
         m = Sector_2022()
         train_baseline(m, ld_train, epoch = 1, batch = 16, fl_rate = 2.0)
@@ -172,7 +174,7 @@ def run_explore3():
         dic['E2FL20'].append(test_chain(m, ld_test))
         train_baseline(m, ld_train, epoch = 1, batch = 16, fl_rate = 2.0)
         dic['E3FL20'].append(test_chain(m, ld_test))
-        save_dic()
+        save_dic(PATH)
     for _ in range(3):
         m = Sector_2022()
         train_baseline(m, ld_train, epoch = 1, batch = 16, fl_rate = 5.0)
@@ -181,5 +183,47 @@ def run_explore3():
         dic['E2FL50'].append(test_chain(m, ld_test))
         train_baseline(m, ld_train, epoch = 1, batch = 16, fl_rate = 5.0)
         dic['E3FL50'].append(test_chain(m, ld_test))
-        save_dic()
+        save_dic(PATH)
 
+
+
+def run_explore4():
+    ld_train = read_ld_train()
+    ld_test = read_ld_test()
+    # 25 * 3 * 3 * 4 = 900 (mins) = 15 (hours)
+    for _ in range(3):
+        m = Sector_2022()
+        train(m, ld_train, fl_rate = 0.5, aux_rate = 0.1)
+        dic['E1FL05AUX00'].append(test_chain(m, ld_test))
+        train(m, ld_train, fl_rate = 0.5, aux_rate = 0.1)
+        dic['E2FL05AUX00'].append(test_chain(m, ld_test))
+        train(m, ld_train, fl_rate = 0.5, aux_rate = 0.1)
+        dic['E3FL05AUX00'].append(test_chain(m, ld_test))
+        save_dic()
+    for _ in range(3):
+        m = Sector_2022()
+        train(m, ld_train, fl_rate = 1.0, aux_rate = 0.1)
+        dic['E1FL10AUX00'].append(test_chain(m, ld_test))
+        train(m, ld_train, fl_rate = 1.0, aux_rate = 0.1)
+        dic['E2FL10AUX00'].append(test_chain(m, ld_test))
+        train(m, ld_train, fl_rate = 1.0, aux_rate = 0.1)
+        dic['E3FL10AUX00'].append(test_chain(m, ld_test))
+        save_dic()
+    for _ in range(3):
+        m = Sector_2022()
+        train(m, ld_train, fl_rate = 2.0, aux_rate = 0.1)
+        dic['E1FL20AUX00'].append(test_chain(m, ld_test))
+        train(m, ld_train, fl_rate = 2.0, aux_rate = 0.1)
+        dic['E2FL20AUX00'].append(test_chain(m, ld_test))
+        train(m, ld_train, fl_rate = 2.0, aux_rate = 0.1)
+        dic['E3FL20AUX00'].append(test_chain(m, ld_test))
+        save_dic()
+    for _ in range(3):
+        m = Sector_2022()
+        train(m, ld_train, fl_rate = 5.0, aux_rate = 0.1)
+        dic['E1FL50AUX00'].append(test_chain(m, ld_test))
+        train(m, ld_train, fl_rate = 5.0, aux_rate = 0.1)
+        dic['E2FL50AUX00'].append(test_chain(m, ld_test))
+        train(m, ld_train, fl_rate = 5.0, aux_rate = 0.1)
+        dic['E3FL50AUX00'].append(test_chain(m, ld_test))
+        save_dic()
