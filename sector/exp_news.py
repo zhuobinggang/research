3ca5a48d3fdaadb0d9f378ebce103427cfeddb79
taku <zhuobinggang@gmail.com>
2022-08-09 17:05:06 +0900

    开save

--- sector/exp_news.py
+++ sector/exp_news.py
@@ -3,6 +3,9 @@ from mainichi_paragraph import read_ld_train, read_ld_tests, read_ld_test, read_
 
 SEEDS_FOR_TRAIN = [21, 22, 8, 4, 14, 3, 19, 97, 10, 666]
 
+def save_model(m, name):
+    t.save(m, f'./save/{name}.tch')
+
 def create_model_with_seed(seed):
     t.manual_seed(seed)
     m = Sector_2022()
@@ -18,15 +21,19 @@ def train_and_save(start = 0, times = 10):
         m = create_model_with_seed(SEED)
         for i in range(2): # AUX_FL
             train(m, ld_train, fl_rate = 5.0, aux_rate = 0.1)
+        save_model(m, f'SEED{SEED}_AUX01FL50E2.tch')
         m = create_model_with_seed(SEED)
         for i in range(2): # AUX
             train(m, ld_train, fl_rate = 0, aux_rate = 0.2)
+        save_model(m, f'SEED{SEED}_AUX02E2.tch')
         m = create_model_with_seed(SEED)
         for i in range(3): # FL
             train_baseline(m, ld_train, fl_rate = 2.0)
+        save_model(m, f'SEED{SEED}_FL20E3.tch')
         m = create_model_with_seed(SEED)
         for i in range(2): # STAND
             train_baseline(m, ld_train, fl_rate = 0)
+        save_model(m, f'SEED{SEED}_STDE3.tch')
 
 
 # NOTE: 新闻数据集的最佳参数和小说不一样！
