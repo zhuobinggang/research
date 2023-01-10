f16eedcc9ea349130ce1c121b0f7584639b2fcd2
taku <zhuobinggang@gmail.com>
2023-01-10 14:41:44 +0900

    尝试深入分析attention

--- sector/case_study.py
+++ sector/case_study.py
@@ -1,5 +1,5 @@
 from exp_news import *
-from case_study_novel import view_att, rank
+from case_study_novel import view_att, rank, rank_by_p, filter_special_tokens
 
 # tests = read_ld_tests()
 # news_test = tests[3]
@@ -27,4 +27,12 @@ def get_case2(labels, y1, y2, testset):
         if label == 1 and auxfl < 0.5 and fl > 0.5:
             res.append((fl - 0.5 + 0.5 - auxfl, auxfl, fl, case))
     return res
+
+### 
+
+def run():
+    seed = 97
+    m_aux = load_model(f'SEED{seed}_AUX01FL50E2')
+    m_fl = load_model(f'SEED{seed}_FL20E3')
+    return rank(m_aux, testds, stand = False), rank(m_fl, testds, stand = True)
 
--- sector/case_study_novel.py
+++ sector/case_study_novel.py
@@ -31,7 +31,7 @@ def get_case2(labels, y1, y2, testset):
 
 ### 可视化注意力 ###
 @t.no_grad()
-def view_att(m, ss, stand = False):
+def view_att(m, ss, stand = False, need_p = False):
   # ss, labels = row
   toker = m.toker
   bert = m.bert
@@ -41,7 +41,8 @@ def view_att(m, ss, stand = False):
   else:
     combined_ids, sep_idxs = encode(ss, toker, [False, True, False, False])
     pool_idx = 0
-  attention = bert(combined_ids.unsqueeze(0).cuda(), output_attentions = True).attentions 
+  out = bert(combined_ids.unsqueeze(0).cuda(), output_attentions = True)
+  attention = out.attentions 
   # tuple of (layers = 12, batch = 1, heads = 12, seq_len = n, seq_len = n)
   need = t.stack(attention) # (12, 1, 12, n, n)
   need = need.mean(0) # (1, 12, n, n)
@@ -50,24 +51,44 @@ def view_att(m, ss, stand = False):
   need = need[pool_idx] # (n)
   atts = need.tolist()
   tokens = toker.convert_ids_to_tokens(combined_ids)
-  return tokens, atts
+  # 同时输出p
+  if need_p:
+    last_hidden_state = out.last_hidden_state
+    pooled_vector = last_hidden_state[0, pool_idx] # (768)
+    p = m.classifier(pooled_vector).item() # p
+    return p, tokens, atts
+  else:
+    return tokens, atts
 
-# m = load_model(f'SEED_19_AUX01FL50E2_6')
-# m = load_model(f'SEED_19_AUX01FL50E2_6')
-def run():
-    m = load_model(f'SEED_19_AUX01FL50E2_6')
-    ss, labels = novel_test[1]
-    view_att_my(m, ss)
 
+def filter_special_tokens(wc):
+    return [word for word in wc if word not in ['[SEP]','[CLS]']]
 
-def rank(m, stand = False):
+def rank(m, test_ds, stand = False):
     wordcloud = []
-    for ss, labels in novel_test:
+    for ss, labels in test_ds:
         tokens, atts = view_att(m, ss, stand)
         dd = list(reversed(sorted(zip(tokens, atts), key = lambda x: x[1])))
         best10 = [token for token, att in dd[:10]]
         wordcloud += best10
     return wordcloud
+
+def filter_tokens_and_pick10(ps_tokens_atts, split = True):
+    wc = []
+    for p, tokens, atts in ps_tokens_atts:
+        if (split and p > 0.5) or (not split and p < 0.5):
+            dd = list(reversed(sorted(zip(tokens, atts), key = lambda x: x[1])))
+            best10 = [token for token, att in dd[:10]]
+            wc += best10
+    return wc
+
+def rank_by_p(m, test_ds, stand = False):
+    ps_tokens_atts = []
+    for ss, labels in test_ds:
+        ps_tokens_atts.append(view_att(m, ss, stand, need_p = True))
+    wc1 = filter_tokens_and_pick10(ps_tokens_atts, split = True)
+    wc0 = filter_tokens_and_pick10(ps_tokens_atts, split = False)
+    return wc1, wc0
 
 # script
 def generate_word_cloud():       
@@ -77,3 +98,8 @@ def generate_word_cloud():
     wordcloud.generate(' '.join(dd))
     wordcloud.to_file('./aux.jpg')
 
+def run():
+    seed = 97
+    m_aux = load_model(f'SEED_19_AUX01FL50E2_6')
+    m_fl = load_model(f'SEED_19_FL50E2_6')
+    return rank(m_aux, novel_test, stand = False), rank(m_fl, novel_test, stand = True)
